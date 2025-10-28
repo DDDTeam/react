42e3717f5f8c30fbd6b2738dcfc1ed877f808dd3
Robocotik <alexander.starkin@gmail.com>
2025-10-28 18:44:59 +0300

    [feat] add export vdomNode

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import {Component} from './component';
 import {createContext} from './context';
 import {h, hFragment, hString} from './h';
 import {render} from './render';
+import type {VDOMNode} from './types';
 
-export {Component, createContext, h, hFragment, hString, render};
+export {Component, createContext, h, hFragment, hString, render, VDOMNode};
