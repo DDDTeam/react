82899ebfbae10db02cbaa8dcb9662d605ad2ded7
Robocotik <alexander.starkin@gmail.com>
2025-10-28 23:37:31 +0300

    [choire] revome VDOMNode import

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,5 @@ import {Component} from './component';
 import {createContext} from './context';
 import {h, hFragment, hString} from './h';
 import {render} from './render';
-import type {VDOMNode} from './types';
-
-export type {VDOMNode};
 
 export {Component, createContext, h, hFragment, hString, render};
